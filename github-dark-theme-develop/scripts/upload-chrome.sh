64434835b6039f85b4aeb3b3a5ce608cdff0b558
Vardan Sawhney <vsawhney@uwaterloo.ca>
2020-02-08 17:57:36 -0500

    only messenger

--- github-dark-theme-develop/scripts/upload-chrome.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo "----------------------------------------------------------------------"
-echo "Upload GitHub Dark Theme Chrome extension to Chrome Web Store"
-echo "----------------------------------------------------------------------"
-
-npx shipit chrome ./dist-chrome
--- github-dark-theme-develop/scripts/upload-firefox.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo "----------------------------------------------------------------------"
-echo "Upload GitHub Dark Theme Firefox Add-ons to https://addons.mozilla.org"
-echo "----------------------------------------------------------------------"
-
-npx shipit firefox ./dist-firefox
