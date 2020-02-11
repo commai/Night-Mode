8c82ea7ec027bd9d328c2a2482652368fd89056d
Vardan Sawhney <vsawhney@uwaterloo.ca>
2020-02-10 22:51:29 -0500

    changes

--- update/init.js
+++ update/init.js
@@ -1,2 +1,17 @@
-var settings;
+function darkmode(){
+    
+}
+
+
+
+chrome.runtime.onMessage.addListener(function(request) {
+    if(request.action === 'executeCode') {
+    	console.log("pressed")
+    	darkmode()
+    }
+    if(request.action === 'de-executeCode'){
+
+    	location.reload(false);
+    }
+});
 
